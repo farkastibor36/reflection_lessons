a49a95811079063004fcd3cc6565e8cb4156d056
Tibi <farkas.tibor1105@gmail.com>
2026-03-06 20:03:31 +0100

    rename test class

--- http-request/src/test/java/org/example/RequestCommandFactoryTest.java
+++ http-request/src/test/java/org/example/RequestCommandFactoryTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-public class ApiTest {
+public class RequestCommandFactoryTest {
     private WebTestClient webTestClient;
     private WebTestClient jsonPlaceholderClient;
 
